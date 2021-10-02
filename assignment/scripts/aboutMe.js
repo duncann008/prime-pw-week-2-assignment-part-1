fa08f82fa64ea93445169073843e186788dcfc42
Duncan Nielsen <duncann008@gmail.com>
2021-10-01 23:23:38 -0500

    finished product

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -62,3 +62,4 @@ switch(true){ //Took me a bit to figure out what went into the switch.
   break;
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?")
